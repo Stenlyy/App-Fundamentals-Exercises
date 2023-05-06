1977b35364959ccfe374e43bd6f533636f796e6e
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-05-06 07:18:43 +0200

    remove

--- app_fundamentals_exercises/tasks.py
+++
@@ -1,4 +0,0 @@
-def invert_values():
-    return "Invert Values:\n Write a program that receives a single string containing " \
-           "positive and negative numbers separated by a single space. " \
-           "Print a list c  ontaining the opposite of each number. \n\nExample:\nInput: 1 2 -3 -3 5\nOutput: [-1, -2, 3, 3, -5] "
