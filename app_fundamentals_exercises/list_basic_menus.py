fa675079948311b71c77f72476a0db9e577d1533
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-05-07 00:05:07 +0200

    list besic menus

---
+++ app_fundamentals_exercises/list_basic_menus.py
@@ -0,0 +1,15 @@
+def invert_values():
+    from app_fundamentals_exercises.list_basic import invert_values
+
+
+def multiples_list():
+    from app_fundamentals_exercises.list_basic import multiples_list
+
+
+def football_cards():
+    from app_fundamentals_exercises.list_basic import football_cards
+
+
+def number_beggars():
+    from app_fundamentals_exercises.list_basic import number_beggars
+
